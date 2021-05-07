9fb77a81bf33346ef1dc26c5b7cf4d2b272761f2
Lars17S <efren.garibaldi@gmail.com>
2021-05-06 22:18:51 -0400

    Picker func camera

---
+++ GaleneApp/PictureViewController.swift
@@ -0,0 +1,59 @@
+//
+//  PictureViewController.swift
+//  GaleneApp
+//
+//  Created by user190841 on 5/6/21.
+//
+
+import UIKit
+
+class PictureViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var fotoButton: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        imageView.backgroundColor = .secondarySystemBackground
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func didTapButton(_ sender: UIButton) {
+        let picker = UIImagePickerController()
+        picker.sourceType = .camera
+        picker.allowsEditing = true
+        picker.delegate = self
+        present(picker, animated: true)
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+extension PictureViewController : UIImagePickerControllerDelegate,
+                                  UINavigationControllerDelegate {
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        picker.dismiss(animated: true, completion: nil)
+        
+        guard let image = info[UIImagePickerController.InfoKey.editedImage] as? UIImage else {
+            return
+        }
+        imageView.image = image
+    }
+    
+}
