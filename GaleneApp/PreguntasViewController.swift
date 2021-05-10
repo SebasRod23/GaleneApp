17766e336967e3e3f35891ec29e435a3a2f107e1
SebasRod23 <sebastian_rodriguezg@yahoo.com>
2021-05-09 22:35:34 -0400

    Home navigation bar design changed

---
+++ GaleneApp/PreguntasViewController.swift
@@ -0,0 +1,29 @@
+//
+//  PreguntasViewController.swift
+//  GaleneApp
+//
+//  Created by user190842 on 5/9/21.
+//
+
+import UIKit
+
+class PreguntasViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationController!.navigationBar.tintColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
