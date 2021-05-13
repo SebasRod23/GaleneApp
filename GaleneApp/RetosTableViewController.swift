41f080db37652d384abe7478b4b163a68604a0a3
SebasRod23 <sebastian_rodriguezg@yahoo.com>
2021-05-12 22:52:47 -0400

    Implemented HealthKit functionality

--- GaleneApp/RetosTableViewController.swift
+++ GaleneApp/RetosTableViewController.swift
@@ -4,13 +4,15 @@
 //
 //  Created by user190842 on 4/14/21.
 //
-
 import UIKit
 import HealthKit
+
 class RetosTableViewCell: UITableViewCell {
     @IBOutlet weak var iconLabel: UIImageView!
     @IBOutlet weak var retoLabel: UILabel!
     @IBOutlet weak var completadoButton: UIButton!
+    @IBOutlet weak var progressLabel: UILabel!
+    
     @IBAction func completadoFunc(_ sender: Any) {
     }
 }
@@ -18,75 +20,36 @@ class RetosTableViewCell: UITableViewCell {
 class RetosTableViewController: UITableViewController {
     
     struct MLData: Codable {
-        var MLString:String
+        var MLString: String
     }
     
-    let direccion="http://martinmolina.com.mx/202111/equipo3/data/retos.json"
-    var currTag : String?
-    var nuevoArray:[Any]?
+    let urlData = "http://martinmolina.com.mx/202111/equipo3/data/retos.json"
+    var currTag: String?
+    var data: [Any]?
+    var filteredData: [Any]?
+    
     private var healthStore : Healthstore?
     private var count: Int? = 0
     
-        override func viewDidLoad() {
-            super.viewDidLoad()
-            
-            tableView.delegate = self
-            healthStore = Healthstore()
-            
-            if let documentsPath = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0] as NSString?,
-            let sourcePath = documentsPath.appendingPathComponent("MLData.plist") as NSString?,
-            let dictionary = NSDictionary(contentsOfFile: sourcePath as String) {
-                self.currTag = dictionary["MLString"] as? String
-            }
-            else {
-                self.currTag = "NA"
-            }
-            
-            
-            if let url = URL(string: direccion) {
-                let datosCrudos = try? Data(contentsOf: url)
-                nuevoArray = try! JSONSerialization.jsonObject(with: datosCrudos!) as? [Any]
-                
-                nuevoArray = nuevoArray!.filter {
-                    let elem = ($0 as! [String: Any])
-                    let tags = elem["tags"] as! [String]
-                    return tags.contains(self.currTag!)
-                }
-                
-            } else {
-                // the URL was bad!
-                print("the URL was bad!")
-            }
-            
-            if let healthStore = healthStore{
-                healthStore.requestAuthorization { success in
-                    if success{
-                        healthStore.calcularPasos{
-                            statisticsCollection in if let statisticsCollection = statisticsCollection{
-                                self.updateUIFromStatistics(statisticsCollection)
-                            }
-                        }
-                    }
-                }
-            }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.delegate = self
+        healthStore=Healthstore()
+        self.fetchDataURL()
+        self.loadMLData()
+        self.filterDataArray()
+        if(currTag=="tenis" || currTag=="balon"){
+            self.getPasos()
         }
-    
-    private func updateUIFromStatistics(_ statisticsCollection: HKStatisticsCollection){
-        let startDate = Calendar.current.date(byAdding: .day, value: -7, to: Date())!
-        let endDate = Date()
-
-        statisticsCollection.enumerateStatistics(from:    startDate, to: endDate){ (statistics,
-            stop) in
-            let steps = statistics.sumQuantity()?.doubleValue(for: .count())
-            self.count = Int(steps ?? 0)
-        }
-        print(self.count!)
+        
+        
     }
     
     override func viewDidAppear(_ animated: Bool) {
         self.loadMLData()
         self.filterDataArray()
         tableView.reloadData()
+        
     }
     
     func filterDataArray() {
@@ -116,10 +79,35 @@ class RetosTableViewController: UITableViewController {
             print("Error loading data from URL!")
         }
     }
+    
+    func getPasos(){
+        if let healthStore = healthStore{
+            healthStore.requestAuthorization { success in
+                if success{
+                    healthStore.calcularPasos{
+                        statisticsCollection in if let statisticsCollection = statisticsCollection{
+                            self.updateUIFromStatistics(statisticsCollection)
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+    private func updateUIFromStatistics(_ statisticsCollection: HKStatisticsCollection){
+            let startDate = Calendar.current.date(byAdding: .day, value: -7, to: Date())!
+            let endDate = Date()
 
+            statisticsCollection.enumerateStatistics(from:    startDate, to: endDate){ (statistics,
+                stop) in
+                let steps = statistics.sumQuantity()?.doubleValue(for: .count())
+                self.count = Int(steps ?? 0)
+            }
+        }
+    
     // MARK: - Table view data source
     override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 162
+        return 185
     }
     
     override func numberOfSections(in tableView: UITableView) -> Int {
@@ -140,13 +128,25 @@ class RetosTableViewController: UITableViewController {
         let retoInfo = self.filteredData![indexPath.row] as! [String: Any]
         let strInfo: String = String(retoInfo["descripcion"] as! String)
           
-        let secondsToDelay = 1.0
-        DispatchQueue.main.asyncAfter(deadline: .now() + secondsToDelay) {
-            cell.retoLabel?.text=String(self.count!)
-        }
-        
+
+        cell.retoLabel?.text = strInfo
+        /*
+         let secondsToDelay = 5.0
+                DispatchQueue.main.asyncAfter(deadline: .now() + secondsToDelay) {
+                    print(self.count!)
+                }
+         */
+        cell.retoLabel?.text=strInfo
         cell.completadoButton.tag = indexPath.row
         cell.completadoButton.addTarget(self, action: #selector(goToCongrats(sender:)), for: .touchUpInside)
+        if(currTag=="tenis" || currTag=="balon"){
+            cell.progressLabel?.text=String(self.count!)+" pasos"
+            cell.progressLabel?.isHidden = false
+        } else{
+            cell.progressLabel?.text=""
+            cell.progressLabel?.isHidden = true
+        }
+        
         
         return cell
     }
