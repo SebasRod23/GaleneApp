6be210940100d768c3852481be8ccb1a40b8d0b4
SebasRod23 <sebastian_rodriguezg@yahoo.com>
2021-05-21 13:47:35 -0400

    WatchOS app added

---
+++ GaleneWatch WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,26 @@
+//
+//  InterfaceController.swift
+//  GaleneWatch WatchKit Extension
+//
+//  Created by user190842 on 5/21/21.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+    }
+
+}
