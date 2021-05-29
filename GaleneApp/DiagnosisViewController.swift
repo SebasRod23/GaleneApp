3dcb88a94f68b724c7fce831b3e5fd507b0a5f19
SebasRod23 <sebastian_rodriguezg@yahoo.com>
2021-05-29 19:58:17 -0400

    getQuestions and saveHistorial implemented

--- GaleneApp/DiagnosisViewController.swift
+++ GaleneApp/DiagnosisViewController.swift
@@ -6,10 +6,18 @@
 //
 
 import UIKit
+import FirebaseFirestore
+import FirebaseAuth
+
 
 class DiagnosisViewController: UIViewController {
 
     var tagResult: String?
+    var diagnosis: String = ""
+    var nretos: Int = 0
+    
+    
+    var db: Firestore!
     
     struct MLData: Codable {
         var MLString: String
@@ -18,11 +26,109 @@ class DiagnosisViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationItem.setHidesBackButton(true, animated: true)
+        // [START setup]
+        let settings = FirestoreSettings()
+
+        Firestore.firestore().settings = settings
+        // [END setup]
+        db = Firestore.firestore()
     }
     
     @IBAction func backToHome(_ sender: UIButton) {
         self.writeToPList()
         _ = navigationController?.popToRootViewController(animated: true)
+        // Query
+        saveData()
+        
+    }
+    
+    struct Reto{
+        var descripcion: String
+        let cumplido: Bool = false
+        let recordatorio: Bool = false
+        
+        func toAnyObject()->Any{
+            return[
+                "descripcion": descripcion,
+                "cumplido": cumplido,
+                "recordatorio": recordatorio
+            ]
+        }
+        
+    }
+    
+    
+    func saveData(){
+        var retos: [String] = []
+        var img: String = ""
+        // Get tags
+        db.collection("retos").whereField("tags", arrayContains: tagResult).getDocuments() { (querySnapshot, err) in
+            if let err = err {
+                print(err)
+            } else {
+                for document in querySnapshot!.documents {
+                    print(document.data())
+                    retos.append(document.get("descripcion") as! String)
+                    img = document.get("imagen") as! String
+                }
+                
+                var retosSelected: [Reto] = []
+                var randoms: [String] = []
+                while self.nretos > 0 {
+                    let reto = Reto(descripcion: retos.randomElement()!)
+                    if (!randoms.contains(reto.descripcion)){
+                        retosSelected.append(reto)
+                        randoms.append(reto.descripcion)
+                        self.nretos-=1
+                    }
+                }
+                
+                var retosAny: [Any] = []
+                for reto in retosSelected {
+                    retosAny.append(reto.toAnyObject())
+                }
+                
+                let user = Auth.auth().currentUser
+                // [END get_user_profile]
+                // [START user_profile]
+                if let user = user {
+                    let uid = user.uid
+                    // Save it on Firebase
+                    self.db.collection("historial").document().setData([
+                        "fecha": Date.init(),
+                        "imagen":img,
+                        "resultado":self.diagnosis,
+                        "retos": retosAny,
+                        "userID": uid
+                    ])
+                }
+                
+                
+            }
+        }
+        /*
+        // Randomly select challenges
+        var retosSelected: [Reto] = []
+        var randoms: [String] = []
+        while nretos > 0 {
+            let reto = Reto(descripcion: retos.randomElement()!)
+            if (!randoms.contains(reto.descripcion)){
+                retosSelected.append(reto)
+                randoms.append(reto.descripcion)
+                nretos-=1
+            }
+        }
+        
+        
+        // Save it on Firebase
+        db.collection("historial").document().setData([
+            "fecha": Date.init(),
+            "imagen":img,
+            "resultado":diagnosis,
+            "retos": retosSelected
+        ])*/
+        
+        
     }
     
     func writeToPList() {
