1ce7ca10c23962f66c123283f077d9e7942e8366
Lars17S <efren.garibaldi@gmail.com>
2021-05-29 23:46:13 -0400

    Fetch data map from firebase

--- GaleneApp/MapaViewController.swift
+++ GaleneApp/MapaViewController.swift
@@ -7,6 +7,8 @@
 import UIKit
 import MapKit
 import CoreLocation
+import Firebase
+import Foundation
 
 class MapaViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
     let locationManager = CLLocationManager()
@@ -14,41 +16,46 @@ class MapaViewController: UIViewController, MKMapViewDelegate, CLLocationManager
     @IBOutlet weak var mapa: MKMapView!
     @IBOutlet weak var direccionLabel: UILabel!
     let direccion="http://martinmolina.com.mx/202111/equipo3/data/ubicaciones.json"
-    var nuevoArray:[Any]?
+    var nuevoArray:[[String: Any]] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        if let url = URL(string: direccion) {
-                //let contents = try String(contentsOf: url)
-                //print(contents)
-                let datosCrudos = try? Data(contentsOf: url)
-                nuevoArray = try! JSONSerialization.jsonObject(with: datosCrudos!) as? [Any]
-            
-        } else {
-            // the URL was bad!
-            print("the URL was bad!")
-        }
-        
-        //where do i look for my methods, in self
         locationManager.delegate=self
         mapa.delegate = self
-        //which is the accuracy you want
         locationManager.desiredAccuracy=kCLLocationAccuracyBestForNavigation
         locationManager.requestWhenInUseAuthorization()
+        let db = Firestore.firestore()
+        db.collection("ubicaciones").getDocuments()	 { (querySnapshot, err) in
+            if let err = err {
+                print("Error getting documents: \(err)")
+            } else {
+                for document in querySnapshot!.documents {
+                    self.nuevoArray.append(document.data())
+                }
+                self.setMapInfo()
+            }
+        }
+
+    }
+    
+    func setMapInfo() {
         var index=0
-        for location  in nuevoArray! {
-            
+        for location  in nuevoArray {
+            print(location)
             let objeto = location as! [String: Any]
-            //let annotation = MKPointAnnotation()
             let s:String = objeto["nombre"] as! String
-            let s2:Double = objeto["longitud"]  as! Double
-            let s3:Double = objeto["latitud"]  as! Double
+            guard let numStr1 = objeto["latitud"]! as? Double else {
+                 return
+            }
+            guard let numStr2 = objeto["longitud"]! as? Double else {
+                return
+            }
+            let c1 : Double = numStr1 as Double
+            let c2 : Double = numStr2 as Double
+            print(c1, c2)
             let dir:String = objeto["direccion"] as! String
             let tel:String = objeto["telefono"] as! String
-            let cl=CLLocationCoordinate2D(latitude: s3 , longitude: s2 )
-            //annotation.title = s
-            //annotation.coordinate = cl
-            //annotation.subtitle = objeto["direccion"]! as! String
+            let cl=CLLocationCoordinate2D(latitude: c1 , longitude: c2 )
             let pointAnnotation = Annotation()
             pointAnnotation.index = index
             pointAnnotation.coordinate = cl
@@ -58,25 +65,16 @@ class MapaViewController: UIViewController, MKMapViewDelegate, CLLocationManager
             pointAnnotation.info = dir
 
             mapa.addAnnotation(pointAnnotation)
-            //direccionLabel.text = objeto["direccion"]! as! String
-            //telefono.text = objeto["telefono"]! as! String
             index+=1
         }
-        
         mapa.showsCompass=true
         mapa.showsScale=true
         mapa.isZoomEnabled=true
         mapa.mapType = MKMapType.hybrid
         let punto = CLLocationCoordinate2DMake(16.782412,-93.1277864)
         mapa.region = MKCoordinateRegion(center: punto, latitudinalMeters: 10000, longitudinalMeters: 10000)
-
- 
-        
-        // Do any additional setup after loading the view.
-
     }
 
-    //user authorizario
     func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
             if status == .authorizedWhenInUse{
                 locationManager.startUpdatingLocation()
