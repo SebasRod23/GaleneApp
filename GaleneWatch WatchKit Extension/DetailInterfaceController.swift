5381e3d4ab22ad666768590c19a74400052d4d9f
Gina <gina10111@hotmail.com>
2021-06-06 20:45:31 -0400

    appleWatch tableView

---
+++ GaleneWatch WatchKit Extension/DetailInterfaceController.swift
@@ -0,0 +1,30 @@
+//
+//  DetailInterfaceController.swift
+//  GaleneWatch WatchKit Extension
+//
+//  Created by user186969 on 6/6/21.
+//
+
+import WatchKit
+import Foundation
+
+
+class DetailInterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
