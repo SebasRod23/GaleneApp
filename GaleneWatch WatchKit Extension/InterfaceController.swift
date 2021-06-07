ee2d26efb64b77021a832f8b330ca47a74992cb6
Gina <gina10111@hotmail.com>
2021-06-06 20:45:31 -0400

    appleWatch tableView

--- GaleneWatch WatchKit Extension/InterfaceController.swift
+++ GaleneWatch WatchKit Extension/InterfaceController.swift
@@ -10,11 +10,22 @@ import Foundation
 
 
 class InterfaceController: WKInterfaceController {
-
+    @IBOutlet weak var tableView: WKInterfaceTable!
+    var retos = ["jkflds","fsdjlkf;das","jkjdfkajal"]
     override func awake(withContext context: Any?) {
         // Configure interface objects here.
+        super.awake(withContext: context)
+        setupTable()
     }
-    
+    func setupTable() {
+        tableView.setNumberOfRows(retos.count, withRowType: "TableClass")
+        
+        for i in 0...retos.count-1{
+            if let row = tableView.rowController(at: i) as? TableClass{
+                row.labelTable.setText(retos[i])
+            }
+        }
+    }
     override func willActivate() {
         // This method is called when watch view controller is about to be visible to user
     }
---
+++ GaleneWatch WatchKit Extension/TableClass.swift
@@ -0,0 +1,13 @@
+//
+//  TableClass.swift
+//  GaleneWatch WatchKit Extension
+//
+//  Created by user186969 on 6/6/21.
+//
+
+import WatchKit
+
+class TableClass: NSObject {
+
+    @IBOutlet weak var labelTable: WKInterfaceLabel!
+}
