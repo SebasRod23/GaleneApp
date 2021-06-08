32efc4cf9872dea9827b23e634c73f1611b87f81
SebasRod23 <sebastian_rodriguezg@yahoo.com>
2021-06-07 22:51:10 -0400

    Watch frontend finished

--- GaleneWatch WatchKit Extension/InterfaceController.swift
+++ GaleneWatch WatchKit Extension/InterfaceController.swift
@@ -10,27 +10,38 @@ import Foundation
 
 
 class InterfaceController: WKInterfaceController {
+    
+    struct Data {
+        var cumplido: Bool
+        var descripcion: String
+        var fecha: String
+        
+    }
+    
     @IBOutlet weak var tableView: WKInterfaceTable!
-    var retos = ["reto1","reto2","reto3"]
-    var fecha1 = ["12/04/21","12/07/20","21/03/21"]
-    var type = ["sports","art","music"]
-    var detail = ["NA","NA","5"]
+    var retos: [Data] = []
     override func awake(withContext context: Any?) {
         // Configure interface objects here.
         super.awake(withContext: context)
+        retos.append(Data(cumplido: false, descripcion: "Pintar mandala", fecha: "07-06-2021"))
+        retos.append(Data(cumplido: true, descripcion: "Haz mucho mucho mucho mucho mucho ejercicio", fecha: "07-06-2021"))
+        retos.append(Data(cumplido: false, descripcion: "Escucha musica", fecha: "07-06-2021"))
+        
+        
         setupTable()
     }
     func setupTable() {
         tableView.setNumberOfRows(retos.count, withRowType: "TableClass")
         
+        
         for i in 0...retos.count-1{
             if let row = tableView.rowController(at: i) as? TableClass{
-                row.labelTable.setText(retos[i])
+                row.labelTable.setText("Reto #"+String(i+1))
             }
         }
     }
     override func table(_ table: WKInterfaceTable, didSelectRowAt rowIndex: Int) {
-        let everything = ["reto" : retos[rowIndex], "date" : fecha1[rowIndex], "type" : type[rowIndex], "detail" : detail[rowIndex]]
+        let everything = ["titulo":"Reto #"+String(rowIndex+1),"reto" : retos[rowIndex].descripcion, "fecha" : retos[rowIndex].fecha, "cumplido" : retos[rowIndex].cumplido] as [String : Any]
         pushController(withName: "showDetails", context: everything)
     }
     override func willActivate() {
